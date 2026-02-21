bfd03db62864db16259d8bfa397fec8776bd9b39
RaghadAlbeladi1 <Raghadz_5@hotmail.com>
2026-02-21 20:39:06 +0000

    put matrix transpose

--- 2-size_me_please.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-def matrix_shape(matrix):
-    shape = []
-    while isinstance(matrix, list):
-        shape.append(len(matrix))
-        matrix = matrix[0]
-    return shape
