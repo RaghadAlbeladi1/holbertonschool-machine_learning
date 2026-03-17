3991a317c20ed9555eb9f76a003bec5ab6645e1a
Raghad Albeladi <Raghadz_5@hotmail.com>
2026-03-17 10:56:49 +0300

    Refactor docstrings for clarity in hierarchy function

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-"""Module that concatenates two DataFrames with a MultiIndex, 
+"""Module that concatenates two DataFrames with a MultiIndex,
 with Timestamp first."""
-
 import pandas as pd
 index = __import__('10-index').index
 
 
 def hierarchy(df1, df2):
-    """Concatenate bitstamp and coinbase DataFrames with Timestamp 
-    as first index level for timestamps 1417411980 to 1417417980, 
+    """Concatenate bitstamp and coinbase DataFrames with Timestamp
+    as first index level for timestamps 1417411980 to 1417417980,
     inclusive.
 
     Args:
@@ -16,24 +15,14 @@ def hierarchy(df1, df2):
         df2: pandas DataFrame (bitstamp)
 
     Returns:
-        pandas.DataFrame: concatenated DataFrame with MultiIndex 
+        pandas.DataFrame: concatenated DataFrame with MultiIndex
         [Timestamp, source]
     """
-    # Index both DataFrames on Timestamp
     df1 = index(df1)
     df2 = index(df2)
-
-    # Select rows within the timestamp range
     df1 = df1.loc[1417411980:1417417980]
     df2 = df2.loc[1417411980:1417417980]
-
-    # Concatenate df2 on top of df1 with keys
     df_concat = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
-
-    # Swap levels to make Timestamp the first level
     df_concat = df_concat.swaplevel(0, 1)
-
-    # Sort by Timestamp to ensure chronological order
     df_concat = df_concat.sort_index(level=0)
-
     return df_concat
