e0c576e9959462552a405392d96901fb3bd8f8ad
Raghad Albeladi <Raghadz_5@hotmail.com>
2026-03-17 10:59:38 +0300

    Refactor docstrings and clean up comments
    Updated docstrings for clarity and removed unnecessary comments.

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-"""Module that concatenates two DataFrames with keys and indexed on Timestamp"""
-
+"""Module that concatenates two DataFrames with keys
+and indexed on Timestamp."""
 import pandas as pd
 index = __import__('10-index').index
 
+
 def concat(df1, df2):
-    """Concatenate df2 rows up to timestamp 1417411920 above df1
+    """Concatenate df2 rows up to timestamp 1417411920 above df1.
 
     Args:
         df1: pandas DataFrame (coinbase)
@@ -14,14 +15,8 @@ def concat(df1, df2):
     Returns:
         pandas.DataFrame: concatenated DataFrame with keys
     """
-    # Index both DataFrames on Timestamp
     df1 = index(df1)
     df2 = index(df2)
-
-    # Select rows from df2 up to timestamp 1417411920
     df2 = df2.loc[:1417411920]
-
-    # Concatenate df2 on top of df1 with keys
     df_concat = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
-
     return df_concat
