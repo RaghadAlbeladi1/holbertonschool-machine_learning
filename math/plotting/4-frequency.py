a947f370250d5aed783f334c8f87a70d5b952686
Raghad Albeladi <Raghadz_5@hotmail.com>
2026-04-05 08:03:59 +0300

    Adjust histogram bins and limits for student grades

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Plot a histogram of student grades for Project A."""
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,10 +8,12 @@ def frequency():
     """Plot histogram of student grades with bins of 10 and black edges."""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
+    student_grades = np.clip(student_grades, 0, 100)
     plt.figure(figsize=(6.4, 4.8))
-    # Plot histogram with bins every 10 units and black edges
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor="black")
+    plt.hist(student_grades, bins=range(0, 110, 10), edgecolor="black")
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title("Project A")
+    plt.xticks(range(0, 110, 10))
+    plt.xlim(0, 100)
     plt.show()
